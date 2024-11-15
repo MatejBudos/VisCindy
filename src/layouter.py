6c397f40d9b76ca5ed842f2be21d5515ef2101a6
MatejBudos <budos.matej10@gmail.com>
2024-11-15 13:27:41 +0100

    add flask

--- src/layouter.py
+++ src/layouter.py
@@ -2,21 +2,31 @@ import igraph as ig
 import matplotlib.pyplot as plt
 import numpy as np
 import json
-class Layouter:
-    def __init__( self, vertices, edges ) -> None:
-        
-        
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask( __name__ )
+api = Api( app )
+
+class Layouter( Resource ):
+    def __init__( self, vertices = [], edges = [] ) -> None:
         self.g = ig.Graph()
         self.g.add_vertices( vertices )
         self.g.add_edges( edges )
         self.nodesNum = self.g.vcount()
-        self.layout_functions = {
+        self.layout_functions = { 
         "grid": self.g.layout_grid_3d,
         "sphere": self.g.layout_sphere,
         "kk": self.g.layout_kamada_kawai_3d,
         "fr": self.g.layout_fruchterman_reingold_3d,
         "random": self.g.layout_random_3d
         }
+
+    def set_graph( self, vertices, edges ) -> None:
+        self.g.clear()
+        self.g.add_vertices( vertices )
+        self.g.add_edges( edges )
+
     def layout( self, layout_type : str = "random") -> None:
         layout_function = self.layout_functions.get(layout_type, self.g.layout_random_3d)
         layout = layout_function()
@@ -35,14 +45,25 @@ class Layouter:
                                     "end": target[ "coords" ] }
         return edges
 
+    def get( self, layout_type : str ):
+        if self.g.vcount() == 0:
+            self.set_graph([i for i in range( 15 )], 
+                        [(0, 1), (0, 2), (0, 3), (1, 2), 
+                       (1, 4), (2, 5),(3, 6), (4, 7), 
+                       (5, 8), (6, 9), (7, 10), (8, 11),
+                       (9, 12), (10, 13), (11, 14), (12, 13), 
+                       (13, 14),(3, 7), (5, 10), (8, 12) ])
 
+        self.layout( layout_type )
+        return self.export()
     def export( self ):
         data = { "nodes":{}, "edges":{} }
         for node in self.g.vs:
             data[ "nodes" ][ node.index ] = { "coords": node["coords"] }
-        data["edges"] = self.route_edges()
-        with open("graph_data.json", "w") as f:
-            json.dump(data, f, indent=3)
+       
+        data[ "edges" ] = self.route_edges()
+        #with open("graph_data.json", "w") as f:
+        #   json.dump(data, f, indent=3)
     
         return data
 
@@ -68,25 +89,12 @@ class Layouter:
 
 
 
-
+api.add_resource( Layouter, "/api/<string:layout_type>" )
 
 
 
 if __name__ == "__main__":
-    l = Layouter([i for i in range( 15 )],
-                        [(0,1),(1,2),(2,3),(3,4),(4,5),(5,6),
-                       (6,7),(7,8),(8,9),(9,10),(10,11),(11,12),
-                       (12,13),(13,14),(14,0)])
-    
-    l = Layouter([i for i in range( 15 )], 
-                        [(0, 1), (0, 2), (0, 3), (1, 2), 
-                       (1, 4), (2, 5),(3, 6), (4, 7), 
-                       (5, 8), (6, 9), (7, 10), (8, 11),
-                       (9, 12), (10, 13), (11, 14), (12, 13), 
-                       (13, 14),(3, 7), (5, 10), (8, 12) ])
-    l.layout("sphere")
-    l.draw()
-    l.export()
+    app.run( debug = True )
   
 
   
