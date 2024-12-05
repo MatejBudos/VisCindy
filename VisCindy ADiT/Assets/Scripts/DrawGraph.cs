21a8dfc0b1298e047a4278494a2a5ddbeeda7f4b
JozefKolek <100275065+JozefKolek@users.noreply.github.com>
2024-12-06 00:19:18 +0100

    string api

--- VisCindy ADiT/Assets/Scripts/DrawGraph.cs
+++ VisCindy ADiT/Assets/Scripts/DrawGraph.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
 
+[System.Serializable]
+public class QueryPayload
+{
+    public string query;
+}
 public class DrawGraph : MonoBehaviour
 {
     public GameObject graphPrefab;
@@ -12,7 +18,7 @@ public class DrawGraph : MonoBehaviour
     public int pregeneratedEdges = 50;
     private int counter = 0;
 
-    public string apiUrl = "http://127.0.0.1:5000/api/grid";
+    public string apiUrl = "http://127.0.0.1:5000/api/";
     private Dictionary<string, NodeObject> _nodesDictionary = new Dictionary<string, NodeObject>();
 
     private bool _loadedFlag = true;
@@ -49,27 +55,103 @@ public class DrawGraph : MonoBehaviour
 
     IEnumerator GetGraphData()
     {
-        using UnityWebRequest www = UnityWebRequest.Get(apiUrl);
-        yield return www.SendWebRequest();
+        string q = @"MATCH (n)
+            OPTIONAL MATCH(n)-[r]->(m)
+            WITH
+            Id(n) AS id,
+            collect(CASE
+                WHEN m IS NOT NULL THEN { source: Id(n), target: Id(m), relationship: type(r)}
+                ELSE NULL
+            END) AS edges
+            RETURN
+            id,
+            [edge IN edges WHERE edge IS NOT NULL] AS edges;";
+        QueryPayload queryPayload = new QueryPayload { query = q };
+        string jsonPayload = JsonUtility.ToJson(queryPayload);
+        //Debug.Log(jsonPayload);
+        //UnityWebRequest www = new UnityWebRequest(apiUrl + "query", "POST");
+        //byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonPayload);
+        //www.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        //www.downloadHandler = new DownloadHandlerBuffer();
+        //www.SetRequestHeader("Content-Type", "application/json");
+        //yield return www.SendWebRequest();
+        using (UnityWebRequest request = new UnityWebRequest(apiUrl + "query", "POST"))
+        {
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonPayload);
+            request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
 
-        if (www.result != UnityWebRequest.Result.Success)
-        {
-            Debug.LogError(www.error);
-        }
-        else
-        {
-            string json = www.downloadHandler.text;
-            Dictionary<string, NodeObject> forAdd = new Dictionary<string, NodeObject>();
-             foreach(var node in ReadingJson.ReadJson(json))
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.Success)
             {
-                Debug.Log("vrojor " + counter);
-                _nodesDictionary.Add(counter.ToString(), node.Value);
-                forAdd.Add(counter.ToString(), node.Value);
-                counter++;
-            }
+                Debug.Log("Query Response: " + request.downloadHandler.text);
 
-            _loadedFlag = true;
-            VisualizeGraph(forAdd);
+                // Pass the response JSON to the next API
+                StartCoroutine(SendToLayouterApi(request.downloadHandler.text));
+            }
+            else
+            {
+                Debug.LogError("Query API Error: " + request.error);
+            }
+        }
+    
+
+        //if (www.result != UnityWebRequest.Result.Success)
+        //{
+        //    Debug.LogError(www.error);
+        //}
+        //else
+        //{
+            
+        //    string json = www.downloadHandler.text;
+        //    Dictionary<string, NodeObject> forAdd = new Dictionary<string, NodeObject>();
+        //    foreach (var node in ReadingJson.ReadJson(json))
+        //    {
+        //        Debug.Log("vrojor " + counter);
+        //        _nodesDictionary.Add(counter.ToString(), node.Value);
+        //        forAdd.Add(counter.ToString(), node.Value);
+        //        counter++;
+        //    }
+
+        //    _loadedFlag = true;
+        //    VisualizeGraph(forAdd);                        
+        //}
+    }
+
+    private IEnumerator SendToLayouterApi(string jsonResponse)
+    {
+
+        using (UnityWebRequest request = new UnityWebRequest(apiUrl + "layouter", "POST"))
+        {
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonResponse);
+            request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log("Layouter Response: " + request.downloadHandler.text);
+                string json = request.downloadHandler.text;
+                Dictionary<string, NodeObject> forAdd = new Dictionary<string, NodeObject>();
+                foreach (var node in ReadingJson.ReadJson(json))
+                {
+                    Debug.Log("vrojor " + counter + " " + node.Key);                    
+                    _nodesDictionary.Add(counter.ToString(), node.Value);
+                    forAdd.Add(counter.ToString(), node.Value);
+                    counter++;
+                }
+
+                _loadedFlag = true;
+                VisualizeGraph(forAdd);
+            }
+            else
+            {
+                Debug.LogError("Layouter API Error: " + request.error);
+            }
         }
     }
 
@@ -96,7 +178,10 @@ public class DrawGraph : MonoBehaviour
                 if (edge != null)
                 {
                     edge.transform.SetParent(gameObject.transform);
-                    _nodesDictionary[node.Key].UIedges.Add(targetNode, edge);
+                    if (!_nodesDictionary[node.Key].UIedges.ContainsKey(targetNode)){
+                        _nodesDictionary[node.Key].UIedges.Add(targetNode, edge);
+                    }
+                    
                 }
             }
         }
--- VisCindy ADiT/Assets/Scripts/ReadingJson.cs
+++ VisCindy ADiT/Assets/Scripts/ReadingJson.cs
@@ -11,58 +11,53 @@ public class ReadingJson
         Dictionary<string, NodeObject> nodesDictionary = new Dictionary<string, NodeObject>();
 
         var data = JsonConvert.DeserializeObject<JObject>(jsonContent);
-        
-        if (data.ContainsKey("data"))
+                
+        if (data["nodes"] is JObject nodes)
         {
-            if (data["data"]?["nodes"] is JObject nodes)
+            foreach (var node in nodes)
             {
-                foreach (var node in nodes)
+                string nodeId = node.Key;
+                if (node.Value is JObject nodeData && nodeData.ContainsKey("coords"))
                 {
-                    string nodeId = node.Key;
-                    if (node.Value is JObject nodeData && nodeData.ContainsKey("coords"))
+                    if (nodeData["coords"] is JArray coords)
                     {
-                        if (nodeData["coords"] is JArray coords)
-                        {
-                            nodesDictionary.Add(nodeId, 
-                                new NodeObject(nodeId, (float)coords[0], (float)coords[1], (float)coords[2]));
-                        }
+                        nodesDictionary.Add(nodeId, 
+                            new NodeObject(nodeId, (float)coords[0], (float)coords[1], (float)coords[2]));
                     }
+                }
+            }
+        }
+                
+        if (data["edges"] is JObject edges)
+        {
+            foreach (var edge in edges)
+            {
+                // string edgeID = edge.Key;
+                var start = edge.Value?["start"] as JArray;
+                var end = edge.Value?["end"] as JArray;
+                string nodeStart = " ";
+                string nodeEnd = " ";
+                foreach (KeyValuePair<string,NodeObject> node in nodesDictionary)
+                {
+                    if(Math.Abs((float) start?[0] - node.Value.x) < Tolerance && 
+                        Math.Abs((float) start?[1] - node.Value.y) < Tolerance && 
+                        Math.Abs((float) start?[2] - node.Value.z) < Tolerance)
+                    {
+                        nodeStart = node.Key;
+                    } else if (Math.Abs((float) end?[0] - node.Value.x) < Tolerance && 
+                                Math.Abs((float) end?[1] - node.Value.y) < Tolerance && 
+                                Math.Abs((float) end?[2] - node.Value.z) < Tolerance)
+                    {
+                        nodeEnd = node.Key;
+                    }
+                }
+                if(!nodeStart.Equals(" ") && !nodeEnd.Equals(" "))
+                {
+                    nodesDictionary[nodeStart].edges.Add(nodeEnd);
                 }
             }
         }
 
-        if (data.ContainsKey("data"))
-        {
-            if (data["data"]?["edges"] is JObject edges)
-            {
-                foreach (var edge in edges)
-                {
-                    // string edgeID = edge.Key;
-                    var start = edge.Value?["start"] as JArray;
-                    var end = edge.Value?["end"] as JArray;
-                    string nodeStart = " ";
-                    string nodeEnd = " ";
-                    foreach (KeyValuePair<string,NodeObject> node in nodesDictionary)
-                    {
-                        if(Math.Abs((float) start?[0] - node.Value.x) < Tolerance && 
-                           Math.Abs((float) start?[1] - node.Value.y) < Tolerance && 
-                           Math.Abs((float) start?[2] - node.Value.z) < Tolerance)
-                        {
-                            nodeStart = node.Key;
-                        } else if (Math.Abs((float) end?[0] - node.Value.x) < Tolerance && 
-                                   Math.Abs((float) end?[1] - node.Value.y) < Tolerance && 
-                                   Math.Abs((float) end?[2] - node.Value.z) < Tolerance)
-                        {
-                            nodeEnd = node.Key;
-                        }
-                    }
-                    if(!nodeStart.Equals(" ") && !nodeEnd.Equals(" "))
-                    {
-                        nodesDictionary[nodeStart].edges.Add(nodeEnd);
-                    }
-                }
-            }
-        }
         return nodesDictionary;
     }
 
