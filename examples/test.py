b313058c21fa0be6abc5db5da4a2d7073b0bc832
MatejBudos <budos.matej10@gmail.com>
2025-01-23 13:28:40 +0100

    example

--- examples/test.py
+++ examples/test.py
@@ -7,13 +7,35 @@ session = requests.Session()
 query_response = session.get(URL + "graph/1")
 
 if query_response.status_code == 200:
-    print("Query response:", query_response.json())
+    response = query_response.json()
+    print("Query response:", response)
 else:
     print("Query failed with status code:", query_response.status_code)
 
-response = query_response.json()
-print(response)
+
+query = """
+        MATCH (n {graphId: 1})
+        OPTIONAL MATCH (n)-[r]->(m)
+        WITH
+            Id(n) AS id,
+            elementId(n) as NeoId,
+            collect(CASE
+                WHEN m IS NOT NULL THEN {source: Id(n), target: Id(m), relationship: type(r), NeoId: elementId(r)}
+                ELSE null
+            END) AS edges
+        RETURN
+            id, NeoId,
+            [edge IN edges WHERE edge IS NOT NULL] AS edges;
+        """
+payload = {"query":query}
+query_response = session.post(URL + "graph/query", json=payload)
+if query_response.status_code == 200:
+    response = query_response.json()
+    print("Query response:", response)
+else:
+    print("Query failed with status code:", query_response.status_code)
 
 
-with open("graph_data.json", "w") as json_file:
-    json.dump(response, json_file, indent=4)
+
+#with open("graph_data.json", "w") as json_file:
+    #json.dump(response, json_file, indent=4)
